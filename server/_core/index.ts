e0b87f36601df2f53024004c37cb52359c490e03
Diego <dmartinezvergara09@gmail.com>
2026-03-23 17:40:21 -0300

    Load dotenv config conditionally based on environment

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -1,4 +1,7 @@
-import "dotenv/config";
+if (process.env.NODE_ENV !== "production") {
+  await import("dotenv/config");
+}
+
 import express from "express";
 import { createServer } from "http";
 import net from "net";
